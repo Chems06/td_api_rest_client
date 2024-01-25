b27926431161c9f59e49f084a1819d19dc8fda90
jouaiti.chems <chemsjouaiti06@gmail.com>
2024-01-25 15:56:18 +0100

    Post & Get mapping

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 const axios = require('axios');
 
+
+axios.post('http://localhost:8080/equipes', {
+    name: 'equipe4',
+})
+    .then(function (response) {
+        console.log(response.data);
+
+        // Une fois l'équipe 4 créée, ajouter les joueurs associés
+        axios.post('http://localhost:8080/joueurs', {
+            name: 'joueur3',
+            surname: 'joueur3',
+            equipe: {
+                id: response.data.id,
+            }
+        })
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        axios.post('http://localhost:8080/joueurs', {
+            name: 'joueur4',
+            surname: 'joueur4',
+            equipe: {
+                id: response.data.id,
+            }
+        })
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
 axios.get('http://localhost:8080/equipes')
     .then(function (response) {
         // handle success
@@ -25,7 +66,7 @@ axios.get('http://localhost:8080/joueurs')
         console.log(response.data.length + ' joueur(s) trouvée(s) :');
 
         response.data.forEach(joueur => {
-            console.log(` - ${joueur.id}, ${joueur.name}, ${joueur.surname}, ${joueur.equipe}`);
+            console.log(` - ${joueur.id}, ${joueur.name}, ${joueur.surname}, ${JSON.stringify(joueur.equipe)}`);
         });
     })
     .catch(function (error) {
